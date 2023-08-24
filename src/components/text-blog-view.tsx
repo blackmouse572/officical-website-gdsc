09665765b7ceb6e563d79196372cf04327a01fdb
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-08-24 20:13:20 +0700

    refactor: closer horizontal display

--- src/components/text-blog-view.tsx
+++ src/components/text-blog-view.tsx
@@ -1,40 +1,63 @@
 import { Card, CardBody } from '@nextui-org/card';
+import { Image } from '@nextui-org/image';
 import { Link } from '@nextui-org/link';
 import { Blogs, allAuthors } from 'contentlayer/generated';
 import { formatDistanceToNow, parseISO } from 'date-fns';
 import vn from 'date-fns/locale/vi';
 import { Icons } from './icons';
-type Props = {
+type TextBlogViewProps = {
   blog: Blogs;
-} & React.ComponentProps<typeof Card>;
+  isWithImage?: boolean;
+  isWithDescription?: boolean;
+} & React.ComponentProps<typeof Card> & {
+    children?: never;
+  };
 
 function getAuthorByPost(post: Blogs) {
   return allAuthors.find((author) => author.name === post.author);
 }
 
-function TextBlogView({ blog, className, ...props }: Props) {
+function TextBlogView({ blog, className, isWithImage, isWithDescription, ...props }: TextBlogViewProps) {
   const author = getAuthorByPost(blog);
   const timeDistance = formatDistanceToNow(parseISO(blog.date), {
     addSuffix: true,
     locale: vn,
   });
   return (
-    <Card shadow="none" isPressable className="group">
+    <Card shadow="none" isPressable className="group" {...props}>
       <CardBody>
-        <h3 className="font-medium group-hover:underline">{blog.title}</h3>
-        <div className="flex items-center gap-1">
-          <p className="text-gray-500 text-xs ">{timeDistance}</p>
-          <Icons.dot className="text-gray-500" size={12} />
-          <p className="text-gray-500 text-xs">
-            by &nbsp;
-            <Link href={`${author?.slug}`} className="text-blue-500 text-xs">
-              {author?.name}
-            </Link>
-          </p>
-        </div>
+        <section className="flex gap-2">
+          {isWithImage && (
+            <div className="relative aspect-square">
+              <Image
+                src={blog.coverImage}
+                alt={blog.title}
+                className="aspect-square object-cover md:max-w-[4rem]"
+                width={300}
+              />
+            </div>
+          )}
+          <div className="">
+            <h3 className="font-medium group-hover:underline  text-base">{blog.title}</h3>
+
+            {isWithDescription && <p className="text-gray-800 md:text-sm text-xs line-clamp-2">{blog.description}</p>}
+
+            <div className="flex items-center gap-1 mt-2">
+              <p className="text-gray-500 text-xs">{timeDistance}</p>
+              <Icons.dot className="text-gray-500" size={12} />
+              <p className="text-gray-500 text-xs">
+                by &nbsp;
+                <Link href={`${author?.slug}`} className="text-blue-500 text-xs">
+                  {author?.name}
+                </Link>
+              </p>
+            </div>
+          </div>
+        </section>
       </CardBody>
     </Card>
   );
 }
 
 export default TextBlogView;
+export type { TextBlogViewProps };
