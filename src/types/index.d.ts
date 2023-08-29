bbdcc91f49359585b862cf43b3d21c7117ea8ed2
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-08-29 16:44:47 +0700

    refactor: add styles to editor

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -17,3 +17,18 @@ export type SidebarNavItem = Pick<NavItem, 'title' | 'disabled'> &
         items: NavItem[];
       }
   );
+
+declare module '@editorjs/header';
+declare module '@editorjs/list';
+declare module '@editorjs/checklist';
+declare module '@editorjs/quote';
+declare module '@editorjs/image';
+declare module '@editorjs/code';
+declare module '@editorjs/embed';
+declare module '@editorjs/link';
+declare module '@editorjs/marker';
+declare module '@editorjs/raw';
+declare module '@editorjs/paragraph';
+declare module '@editorjs/simple-image';
+declare module '@editorjs/warning';
+declare module '@editorjs/image';
