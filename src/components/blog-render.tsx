600bb5ca8bb24de401570560d07692995d7d0160
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-09-04 11:25:47 +0700

    refactor: edit blog

--- src/components/blog-render.tsx
+++ src/components/blog-render.tsx
@@ -1,7 +1,12 @@
+import { Icons } from '@components/icons';
+import { getSessionServerSide } from '@lib/auth';
+import { Button } from '@nextui-org/button';
+import { Divider } from '@nextui-org/divider';
 import { User as UserDisplay } from '@nextui-org/user';
 import { Post, User } from '@prisma/client';
 import { format } from 'date-fns';
-import Blocks from 'editorjs-blocks-react-renderer';
+import Blocks, { DataProp } from 'editorjs-blocks-react-renderer';
+import Link from 'next/link';
 
 type Props = {
   blog: Post & {
@@ -9,7 +14,25 @@ type Props = {
   };
 };
 
-function BlogRenderer({ blog }: Props) {
+function toDataProp(content: any): DataProp {
+  const b = content as any;
+  if (!b)
+    return {
+      blocks: [],
+      version: '',
+      time: 0,
+    };
+
+  return {
+    blocks: b.blocks || [],
+    version: b.version || '',
+    time: b.time || 0,
+  };
+}
+
+async function BlogRenderer({ blog }: Props) {
+  const user = await getSessionServerSide();
+  const data = toDataProp(blog.content);
   return (
     <div className="prose prose-stone mx-auto">
       <h1 className="">{blog.title}</h1>
@@ -21,11 +44,22 @@ function BlogRenderer({ blog }: Props) {
         }}
         description={
           <div className="flex items-center space-x-2">
-            <span>Publish {format(new Date(blog.updatedAt), 'dd/MM/yyyy')}</span>
+            {blog.published ? 'Published' : 'Draft'} at &nbsp;
+            <span>{format(new Date(blog.updatedAt), 'dd/MM/yyyy')}</span>
           </div>
         }
       />
-      <Blocks data={blog.content as any} />
+      <Blocks data={data} />
+      {blog.author?.email === user?.user.email && (
+        <>
+          <Divider />
+          <Link href={`/editor/${blog.slug}`}>
+            <Button color="primary" variant="flat" startContent={<Icons.pen className="w-4 h-4" />}>
+              Edit
+            </Button>
+          </Link>
+        </>
+      )}
     </div>
   );
 }
